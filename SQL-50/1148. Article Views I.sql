5a5e1c0e94004780cbfbcb294a501f304ee82d7f
SHREYA TRIPATHI <shreyatripathi1808@gmail.com>
2024-11-10 17:44:56 +0530

    Add files via upload

---
+++ SQL-50/1148. Article Views I.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC;
+
+-- select author_id as id
+-- from views
+-- where author_id=viewer_id
+-- group by id
+-- order by id
+
