dbff2cecb839dd1648a922ce9e538a9eec187761
SHREYA TRIPATHI <shreyatripathi1808@gmail.com>
2025-06-14 22:22:19 +0530

    Update 601. Human Traffic of Stadium.sql

--- 601. Human Traffic of Stadium.sql
+++ 601. Human Traffic of Stadium.sql
@@ -1,16 +1,23 @@
-# Write your MySQL query statement below
-WITH CTE AS (
-    SELECT *, id - ROW_NUMBER() OVER(ORDER BY visit_date) AS grp
-    FROM Stadium
-    WHERE people >= 100
-)
+WITH cte AS (
+  SELECT *, ROW_NUMBER() OVER (ORDER BY id) AS row_num, id-ROW_NUMBER() OVER (ORDER BY id) as diff
+  FROM Stadium where people >=100)
 
-SELECT id, visit_date, people
-FROM CTE
-WHERE grp IN (
-    SELECT grp
-    FROM CTE
-    GROUP BY grp
-    HAVING COUNT(grp) >= 3
-)
-ORDER BY visit_date;
+select id,visit_date,people from cte where diff IN (select diff from cte group by diff having count(*) >= 3)
+
+-- -- -----------OR----------- --
+
+-- WITH CTE AS (
+--     SELECT *, id - ROW_NUMBER() OVER(ORDER BY visit_date) AS grp
+--     FROM Stadium
+--     WHERE people >= 100
+-- )
+
+-- SELECT id, visit_date, people
+-- FROM CTE
+-- WHERE grp IN (
+--     SELECT grp
+--     FROM CTE
+--     GROUP BY grp
+--     HAVING COUNT(grp) >= 3
+-- )
+-- ORDER BY visit_date;
