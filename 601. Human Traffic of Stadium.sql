efec6053cb8a7653492c27e1c993de6898a4f153
SHREYA TRIPATHI <shreyatripathi1808@gmail.com>
2025-06-14 22:29:29 +0530

    Update 601. Human Traffic of Stadium.sql

--- 601. Human Traffic of Stadium.sql
+++ 601. Human Traffic of Stadium.sql
@@ -1,3 +1,15 @@
+SELECT id, visit_date, people 
+FROM
+    (SELECT *, COUNT(*) OVER (PARTITION BY consecutive) AS count
+    FROM
+        (SELECT *, id - ROW_NUMBER() OVER (ORDER BY id) AS consecutive
+        FROM Stadium
+        WHERE people >= 100) t )t2
+WHERE count >= 3
+ORDER BY id
+
+-- -- -----------OR----------- --
+
 WITH cte AS (
   SELECT *, ROW_NUMBER() OVER (ORDER BY id) AS row_num, id-ROW_NUMBER() OVER (ORDER BY id) as diff
   FROM Stadium where people >=100)
@@ -6,18 +18,17 @@ select id,visit_date,people from cte where diff IN (select diff from cte group b
 
 -- -- -----------OR----------- --
 
--- WITH CTE AS (
---     SELECT *, id - ROW_NUMBER() OVER(ORDER BY visit_date) AS grp
---     FROM Stadium
---     WHERE people >= 100
--- )
-
--- SELECT id, visit_date, people
--- FROM CTE
--- WHERE grp IN (
---     SELECT grp
---     FROM CTE
---     GROUP BY grp
---     HAVING COUNT(grp) >= 3
--- )
--- ORDER BY visit_date;
+WITH CTE AS (
+    SELECT *, id - ROW_NUMBER() OVER(ORDER BY visit_date) AS grp
+    FROM Stadium
+    WHERE people >= 100
+)
+SELECT id, visit_date, people
+FROM CTE
+WHERE grp IN (
+    SELECT grp
+    FROM CTE
+    GROUP BY grp
+    HAVING COUNT(grp) >= 3
+)
+ORDER BY visit_date;
